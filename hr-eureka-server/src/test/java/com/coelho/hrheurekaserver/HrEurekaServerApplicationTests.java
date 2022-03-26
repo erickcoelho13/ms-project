984cfab2af3c50fe1a8871bfaa60cfdfa6baff3b
Erick Coelho <erck.coelho@gmail.com>
2022-03-26 13:45:39 -0300

    Project hr-eureka-server created

---
+++ hr-eureka-server/src/test/java/com/coelho/hrheurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coelho.hrheurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
