cbd862c3e2a02fb563c8ae7d311634d5650a73a6
Erick Coelho <erck.coelho@gmail.com>
2022-03-26 09:22:50 -0300

    RestTemplate

--- hr-payroll/src/main/java/com/coelho/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/coelho/hrpayroll/services/PaymentService.java
@@ -1,14 +1,33 @@
 package com.coelho.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.coelho.hrpayroll.entities.Payment;
+import com.coelho.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
 
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+
 	public Payment getPayment(long workerId, int days) {
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", ""+workerId);
 		
-		return new Payment("Coelho", 200.0, days);
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
+	
+	
 }
